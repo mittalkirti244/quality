8848f4515c1fcab734f35f39e7db54f6e742a425
Ashutosh Singh <ashusingh.10k@gmail.com>
2021-10-13 05:46:40 +0000

    v1.2

---
+++ srv/manageOrder-service.js
@@ -0,0 +1,10 @@
+module.exports = cds.service.impl(async function() {
+	const { Country,CountryText } = this.entities;
+	const service = await cds.connect.to('country');
+	this.on('READ', Country, request => {
+		return service.tx(request).run(request.query);
+    });
+    	this.on('READ', CountryText, request => {
+		return service.tx(request).run(request.query);
+	});
+});
